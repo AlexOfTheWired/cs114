3ed78f3dcab4ea392478aca310ac171afb8d0292
s203 <s203@ai241lab0203m13.aipdcls.edu>
2017-07-27 15:17:42 -0700

    Week 3

---
+++ greeting.py
@@ -0,0 +1,10 @@
+print("hello, what is your name?")
+name = input()
+
+print("how old are you?")
+age = int(input())
+greeting = 'Nice to meet you, ' + name + '!'
+print(greeting)
+age_next_year = age + 1
+print('Next year you will be ' + str(age_next_year) + 'years old...')
+print('That\'s ancient...')

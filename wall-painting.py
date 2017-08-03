9f7d708d21462441b11874ddb84d54219a2a3766
s203 <s203@ai241lab0203m13.aipdcls.edu>
2017-08-03 15:47:24 -0700

    Week #4

--- wall-painting.py
+++ wall-painting.py
@@ -8,6 +8,7 @@ How much a gallon of paint costs
 Figure out then print how much it will cost to paint the wall with one coat.
 
 """
+from time import sleep
 print(" Width of wall?")
 width  = float(input())
 print(" Height of wall?")
@@ -17,7 +18,17 @@ cost   = float(input())
 
 area             = width * height
 gallon_coverage  = 400
-overall_cost     = (area / gallon_coverage) * cost
+cost_per_gallon  = (area / gallon_coverage) * cost
 
-print("It will  " + str(overall_cost)
- + " moneys to paint wall with one coat." )
+
+print("It will " + str(cost_per_gallon) +
+" moneys to paint wall with one coat.")
+
+sleep(3)
+
+print("Actuall... How many coats of paint?")
+coats = float(input())
+
+cost_with_coats   = cost_per_gallon * coats
+
+print("So. It will cost $" + str(cost_with_coats))

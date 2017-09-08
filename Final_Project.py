aec06448f886291e552fd09a389747e2d1303153
s203 <s203@AI241LAB0203M13.local>
2017-09-07 17:02:55 -0700

    WEEK 9

---
+++ Final_Project.py
@@ -0,0 +1,98 @@
+"""
+Final Project Proposal()
+Create a cash register program.
+
+- On initialize the user sets amount of Bills and Coins in register
+      (starting bank amount)
+
+- Set Sales Tax
+      - sales_tax = 2.3%
+
+- Then print Register status:
+      - Amount of Coins and Bills
+      - Total cash amount
+
+
+Render Transaction:
+    (Start with empty list... dictionary?)
+
+- Input item price
+    ( append price to list... dictionary?)
+    transaction_0001 = {
+                        'Item #1': 3.50,
+                        'Item #2': 11.99,
+                        'Item #3': 2.30,
+                        'Item #4': 23.00
+                        }
+
+- Calculate Sub Total
+    ( )
+
+- Calculate balance due/TOTAL
+    ( use sum() on item list items +  sales_tax)
+
+- Input amount tendered
+    ( input how much of each unit was given )
+
+- Calculate change return
+    - Coins
+    - Bills
+
+Print Receipt
+    FORMAT EXAMPLE
+======================================
+        - ( STORE NAME ) -
+
+      list             price
+    ==========================
+     - Item #1          $3.50
+     - Item #2         $11.99
+     - Item #3          $2.30
+     - Item #4         $23.00
+    --------------------------
+     - Sub Total       $40.79
+     - SALES TAX        $0.00
+
+     - Total           $40.79
+     - Cash            $50.00
+     - CHANGE ===>      $9.23
+    ==========================
+     Transaction Number: 0001
+
+=====================================
+
+- Make function that stores transaction data for later use.
+
+- Complete transaction
+        (reset tranaction dictionary or start new tranaction list.)
+
+- Record amount of coins and bills were exchanged after transaction.
+    - how many coins and bills were taken out of register?
+    - how many coins and bills were taken in after transaction?
+
+Print Register status: (Audit Tally)
+    - All Transactions
+        ( Make function that stores transaction data? Transaction Class!
+          Each transaction creates a new instance. )
+      - Amount of Coins and Bills
+      - Total cash amount
+
+I can even make a GUI for this!
+  !!!GUI IS A MUST NOW!!!
+
+
+(Destroy Register)
+
+Possible Bugs:
+- register runs out of bills or coins
+- customer pays in FUCKING PENNIES!!!
+- change back requires (1 five, 4 singles, 2 dimes, 3 pennies).
+  but register does not have any fives.
+-
+
+"""
+
+# Setup
+# Input
+# Transform
+# Output
